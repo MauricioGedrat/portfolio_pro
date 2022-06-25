20e4f02d767f38ae5c6436d0704e1c20ef10d613
MauricioGedrat <mauriciogedrat@gmail.com>
2022-06-25 14:47:51 -0300

    qdwed

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,27 +1,31 @@
+import HeroSvg from 'common/svgIcons/HeroSvg'
 import Image from 'next/image'
 import React from 'react'
-import Img from '../../assets/images/hero-img.svg'
+
 import GatoImg from '../../assets/images/gato.jpg'
 
 const Hero = () => {
     return (
-        <div className="flex items-center justify-center h-screen">
-            <main className="grid gap-14 xl:grid-cols-2 ">
-                <div>
-                    <Image src={Img} alt="hero image" />
+        <div className="flex items-center justify-center xl:h-screen">
+            <main className="grid mt-10 xl:mt-0 gap-14 xl:grid-cols-2">
+                <div className="flex justify-center">
+                    <HeroSvg />
                 </div>
                 <div>
-                    <h1 className="text-4xl font-kdam">
+                    <h1 className="text-3xl text-center sm:text-4xl font-kdam">
                         Olá, Eu sou Maurício Gedrat
                     </h1>
-                    <main className="bg-[#00000036] p-4 mt-10 rounded-lg">
-                        <span>
+                    <main className="bg-[#00000036] p-4 mt-10 rounded-lg sm:flex gap-20">
+                        <div className="flex justify-center sm:justify-start">
                             <Image
                                 src={GatoImg}
                                 alt="gato"
-                                className="rounded-lg"
+                                className="max-w-full rounded-full sm:rounded-lg "
                             />
-                        </span>
+                        </div>
+                        <div className="flex justify-center sm:justify-start">
+                            efewfeewf
+                        </div>
                     </main>
                 </div>
             </main>
